39ebe3a1528dcb81193d259fce958ffb88702a95
Théo LUDWIG <contact@theoludwig.fr>
2024-03-24 19:39:15 +0100

    feat: list habits basic UI

--- app/application/habits/index.tsx
+++ app/application/habits/index.tsx
@@ -1,24 +1,58 @@
-import { StyleSheet, Text, View } from "react-native"
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6"
+import { FlatList, StyleSheet } from "react-native"
+import { List } from "react-native-paper"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 import { useHabitsTracker } from "@/presentation/react/contexts/HabitsTracker"
+import { colorsPresenter } from "@/presentation/presenters/utils/ColorsPresenter"
 
 const HabitsPage: React.FC = () => {
   const { habitsTracker } = useHabitsTracker()
 
   return (
-    <SafeAreaView style={styles.container}>
-      {habitsTracker.habitsHistory.map((progressHistory) => {
-        const { habit } = progressHistory
+    <SafeAreaView style={[styles.container]}>
+      <List.Section style={[styles.habitsList]}>
+        <FlatList
+          data={habitsTracker.habitsHistory}
+          renderItem={({ item }) => {
+            const { habit } = item
 
-        return (
-          <View key={habit.id}>
-            <Text>
-              {habit.name} ({habit.goal.type})
-            </Text>
-          </View>
-        )
-      })}
+            return (
+              <List.Item
+                title={habit.name}
+                style={[
+                  styles.habitItem,
+                  {
+                    backgroundColor: colorsPresenter.hexToRgbA(
+                      habit.color,
+                      0.4,
+                    ),
+                  },
+                ]}
+                contentStyle={[
+                  {
+                    paddingLeft: 12,
+                  },
+                ]}
+                titleStyle={[
+                  {
+                    fontSize: 18,
+                  },
+                ]}
+                left={() => {
+                  return (
+                    <FontAwesome6
+                      size={24}
+                      name={habit.icon}
+                      style={[styles.habitItemIcon]}
+                    />
+                  )
+                }}
+              />
+            )
+          }}
+        />
+      </List.Section>
     </SafeAreaView>
   )
 }
@@ -27,7 +61,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+  },
+  habitsList: {
+    width: "90%",
+  },
+  habitItem: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  habitItemIcon: {
+    width: 30,
   },
 })
 
