6c9538666689854ff3ea11fa80061da15fdc7f45
Xc165543337 <90028194+Xc165543337@users.noreply.github.com>
2024-04-04 17:06:42 +0200

    feat: add basic habit create form

--- presentation/react/components/HabitCreateForm/HabitCreateForm.tsx
+++ presentation/react/components/HabitCreateForm/HabitCreateForm.tsx
@@ -1,8 +1,15 @@
-import { useForm } from "react-hook-form"
-import { Appbar } from "react-native-paper"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { Appbar, Button, HelperText, TextInput } from "react-native-paper"
 import { SafeAreaView } from "react-native-safe-area-context"
+import ColorPicker, {
+  HueSlider,
+  Panel1,
+  Preview,
+} from "reanimated-color-picker"
 
 import type { HabitCreateData } from "@/domain/entities/Habit"
+import { HabitCreateSchema } from "@/domain/entities/Habit"
 import type { User } from "@/domain/entities/User"
 
 export interface HabitCreateFormProps {
@@ -10,8 +17,20 @@ export interface HabitCreateFormProps {
 }
 
 export const HabitCreateForm: React.FC<HabitCreateFormProps> = ({ user }) => {
-  useForm<HabitCreateData>({
-    // const { control, handleSubmit, setValue } = useForm<HabitCreateData>({
+  // const {createHabit, habitPresenter} = useHabitCreate()
+
+  const onSubmit = async (data: HabitCreateData): Promise<void> => {
+    // await habitPresenter.createHabit(data)
+    console.log(data)
+  }
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<HabitCreateData>({
+    mode: "onChange",
+    resolver: zodResolver(HabitCreateSchema),
     defaultValues: {
       userId: user.id,
       name: "",
@@ -37,7 +56,118 @@ export const HabitCreateForm: React.FC<HabitCreateFormProps> = ({ user }) => {
           }}
         />
       </Appbar.Header>
-      {/* <Controller></Controller> */}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <>
+              <TextInput
+                placeholder="Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                style={[styles.input]}
+                mode="outlined"
+              />
+              {errors.name != null ? (
+                <HelperText type="error" visible>
+                  {errors.name.type === "too_big"
+                    ? "Name is too long"
+                    : "Name is required"}
+                </HelperText>
+              ) : null}
+            </>
+          )
+        }}
+        name="name"
+      />
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => {
+          return (
+            <ColorPicker
+              style={{ width: "70%" }}
+              value={value}
+              onChange={(value) => {
+                onChange(value.hex)
+              }}
+            >
+              <Preview hideInitialColor />
+              <Panel1 />
+              <HueSlider />
+            </ColorPicker>
+          )
+        }}
+        name="color"
+      />
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <TextInput
+              placeholder="Icon"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              style={[styles.input]}
+              mode="outlined"
+            />
+          )
+        }}
+        name="icon"
+      />
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <TextInput
+              placeholder="Goal Frequency"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              style={[styles.input]}
+              mode="outlined"
+            />
+          )
+        }}
+        name="goal.frequency"
+      />
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <TextInput
+              placeholder="Goal Target Type"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              style={[styles.input]}
+              mode="outlined"
+            />
+          )
+        }}
+        name="goal.target.type"
+      />
+
+      <Button
+        mode="contained"
+        onPress={handleSubmit(onSubmit)}
+        // loading={createHabit.state === "loading"}
+        // disabled={createHabit.state === "loading"}
+      >
+        Create your habit
+      </Button>
     </SafeAreaView>
   )
+}
+
+const styles = {
+  input: {
+    margin: 8,
+  },
 }
